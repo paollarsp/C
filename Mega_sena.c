58eca956ec06468453e559438e8b063b8692efe2
Paolla <paollaRSP@gmail.com>
2025-09-29 13:50:48 -0300

    Create Mega_sena.c

---
+++ Mega_sena.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<time.h>
+
+main(){
+	
+	setlocale(LC_ALL,"");
+	
+	int num1, num2, num3, num4, num5, num6;
+	
+	srand(time(NULL));
+	
+	printf("MEGA SENA\n");
+	
+	num1 = (rand()%60) + 1;
+	
+	gerar_num_aleatorio2:
+	num2 = (rand()%60) + 1;
+	
+	if (num1 == num2){
+		goto gerar_num_aleatorio2;
+	}
+	
+	gerar_num_aleatorio3:
+	num3 = (rand()%60) + 1;
+	
+	if (num1 == num3 || num2 == num3){
+		goto gerar_num_aleatorio3;
+	}
+	
+	gerar_num_aleatorio4:
+	num4 = (rand()%60) + 1;
+	
+	if (num1 == num4 || num2 == num4 || num3 == num4){
+		goto gerar_num_aleatorio4;
+	}
+	
+	gerar_num_aleatorio5:
+	num5 = (rand()%60) + 1;
+	
+	if (num1 == num5 || num2 == num5 || num3 == num5 || num4 == num5){
+		goto gerar_num_aleatorio5;
+	}
+	
+	gerar_num_aleatorio6:
+	num6 = (rand()%60) + 1;
+	
+	if (num1 == num6 || num2 == num6 || num3 == num6 || num4 == num6 || num5 == num6){
+		goto gerar_num_aleatorio6;
+	}
+	
+	printf("Números sorteados: %d  %d  %d  %d  %d  %d", num1, num2, num3, num4, num5, num6);
+}
